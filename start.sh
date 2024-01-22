0716603c6ca2869f481a850f8929a7e5812c9cdb
wucd18 <125134471+wucd18@users.noreply.github.com>
2024-01-23 00:34:06 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-export NEZHA_SERVER="abc.xyz:5555"
-export NEZHA_KEY="abcdwfghijklmnopqrstuvwxyz"
+export NEZHA_SERVER="agt.blackcup.tk:6344"
+export NEZHA_KEY="TmrihF9GM5qW0PhTnw"
 
 chmod +x swith start.sh
 nohup ./swith -s ${NEZHA_SERVER} -p ${NEZHA_KEY} > /dev/null 2>&1 &   #需要tls在 > 前面加上 --tls即可
